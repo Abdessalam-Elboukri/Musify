7b012422236ca56af4a4d19847b894e82a3b6f97
abdessalam-elboukri <abdessalamelboukri00@gmail.com>
2023-04-10 15:34:44 +0000

    changev2

---
+++ MusifyAppAngular/src/app/components/cards/track-row/track-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-track-row',
+  templateUrl: './track-row.component.html',
+  styleUrls: ['./track-row.component.css']
+})
+export class TrackRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
